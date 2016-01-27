c44d0c1b435d6b9edcdf3876d00fb493925963be
caneballante <jonbridgman@gmail.com>
2016-01-27 13:23:02 -0800

    SHELLFISH BEDS INDICATOR - FOOTER
    Created a new footer include so which has the google tracking code. For
    some reason it wasn’t included in the old footer. I’m concerned about
    replacing it across all the pages right now so I made a new footer_2016
    to test it out.

--- shellfish_beds_reopened_indicator1.php
+++ shellfish_beds_reopened_indicator1.php
@@ -291,20 +291,7 @@ $(function() {
 			</div>
 		</div>
 	</div>
-	<?php include 'includes/footer.html' ?>
-</div>
-<script type="text/javascript">
+	<?php include 'includes/footer_2016.html' ?>
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-679161-6']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 </body>
 </html>
