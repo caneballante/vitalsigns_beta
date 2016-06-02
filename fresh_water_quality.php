7f4686a6d19aa19ede340540a7dc7815c8ffaa8a
Erika Lari <erika.lari@psp.wa.gov>
2016-06-02 15:03:46 -0700

    freshwater quality

--- fresh_water_quality.php
+++ fresh_water_quality.php
@@ -3,12 +3,12 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta name="google-site-verification" content="lMaE6uVdAY_4P-zfoagi1Kj2QQLS8W3JvahLqMZMuDU" />
-<title>Puget Sound Partnership - Vital Signs - Fresh Water Quality</title>
-<link href="css/vital1.css" rel="stylesheet" type="text/css" />
+<title>Puget Sound Partnership - Vital Signs - Birds</title>
+<link href="css/vital2016.css" rel="stylesheet" type="text/css" />
 <link type="text/css" href="css/custom-theme/jquery-ui-1.9.2.custom.min.css" rel="stylesheet" />
 <script type="text/javascript" src="js/jquery-1.8.3.js"></script>
 <script type="text/javascript" src="js/jquery-ui-1.9.2.custom.min.js"></script>
-<script type="text/javascript" src="js/divhide.js"></script>
+<script type="text/javascript" src="js/settab.js"></script>
 <script>
 $(function() {
 		$( "#accordion" ).accordion( { collapsible: true }, { active: false }, { autoHeight: false });
@@ -21,71 +21,41 @@ $(function() {
   });
 });	
 </script>
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 1;
+	color = "aqua";
+</script>
 </head>
 <body id="id19" >
 <div id="container">
 	<?php include 'includes/header_test.html' ?>
-	<!--*********************************TITLE BAR BEGIN*********************************-->
-	<div id="roundcornerstop"><img src="img/rounded_top.jpg" width="960" height="20" /></div>
-	<div id="vitalheadercontainer">
-		<div id="vitalheaderleftcontainer">
-			<div id="vitaltitle">Freshwater Quality </div>
-			<div id="vitalquote">21% of stream and river segments that were assessed in 2010 were listed as impaired</div>
-			<div id="vitalchampion">Indicator Leads: Markus Van Prause, Washington State Department of Ecology, Ken Koch, Washington State Department of Ecology, and Jo Wilhelm, King County</div>
-			<div id="vitalbuttonscontainer">
-				<div id="button1"><a href="http://www.psp.wa.gov/pugetsoundprojectatlas" target="_blank">View Projects</a></div>
-				<div id="button2"><a href="http://gismanager.rco.wa.gov/ntaportal?ActionAgenda=2014-2016&Quarter=10&Vital%20Sign=Freshwater%20Quality" target="_blank">View Report Card</a></div>
-			</div>
-		</div>
-		<div id="vitalheaderrightphoto">
-			<div class="underconstruction">This webpage is currently being updated. For the most recent findings for this Vital Sign, please consult the report in the latest <a href="https://pspwa.box.com/s/rhb2s6c2yzb2eskla1ssj2w84vcay7un">State of the Sound</a></div>
-			<div class="photoCredit padding-top">Data last updated on March 9, 2015<br />
-			Photo Credit: Dan Bennett</div></div>
-	</div>
-	<div id="roundcornerbottom"><img src="img/rounded_rounded_bottom.jpg" width="960" height="20" /></div>
-	<!--*********************************TITLE BAR END*********************************--> 
-	<!--*********************************TAB BEGIN*********************************-->
+    <?php include 'includes/freshwater_quality-title.html' ?>
+<?php include 'includes/freshwater_quality-tab.html' ?>	
 	
-	<ul id="nav_indicator">
-		<li class="indicator_active_aqua"><a href="fresh_water_quality.php">Vital Sign: Overview<br />
-			<br />
-			<br />
-			</a> </li>
-		<li ><a href="fresh_water_quality_indicator1.php">Indicator: Water Quality Index<br />
-			<br />
-			</a></li>
-		<li><a href="fresh_water_quality_indicator2.php">Indicator: Number of impaired waters<br />
-			<br />
-			</a></li>
-		<li><a href="fresh_water_quality_indicator3.php">Indicator: Benthic Index of Biotic Integrity<br />
-			<br />
-			</a></li>
-	</ul>
-	<div class="clearfloat"></div>
-	<!--*********************************TAB END*********************************-->
 	<div id="accordioncontainer">
 		<div id="accordion"> 
 			
 			<!--*********************************FOLD 01*********************************-->
 			<h3><a href="#!">Importance to Puget Sound recovery</a></h3>
 			<div>
-				<p>The rivers and streams that flow into Puget Sound are the lifeblood of our region’s ecosystems and our health, economy, and quality of life. Yet only 64% of the major rivers in Puget Sound meet water quality goals. </p>
-				<p>Clean water is vital to people and to healthy fish and wildlife populations. When our rivers and streams pick up pollutants, toxic contaminants, or excessive sediments and nutrients, it adversely affects the health of our watersheds, marine waters, swimming beaches, and shellfish beds.</p>
-				<p>Three key indicators help us monitor the health of Puget Sound: the number of impaired waters, the Water Quality Index, and the Benthic Index of Biotic Integrity. Under the federal Clean Water Act of 1972, waters that fail to meet water quality standards are considered impaired. The Water Quality Index integrates complex water quality data into a readily understood scale. The Benthic Index of Biotic Integrity measures the abundance and diversity of macroinvertebrates in a streambed. Also known as stream bugs, these creatures are a critical part of the aquatic food web and are sensitive to changes in the environment.</p>
-			</div>
+				<p>The rivers and streams that flow into Puget Sound are the lifeblood of our region&rsquo;s ecosystems and our health, economy, and quality of life. Yet only 64% of the major rivers in Puget Sound meet water quality goals.</p>
+				  <p>Clean water is vital to people and to healthy fish and wildlife populations. When our rivers and streams pick up pollutants, toxic contaminants, or excessive sediments and nutrients, it adversely affects the health of our watersheds, marine waters, swimming beaches, and shellfish beds.</p>
+			    <p>Three key indicators help us monitor the health of Puget Sound: the number of impaired waters, the Water Quality Index, and the Benthic Index of Biotic Integrity. Under the federal Clean Water Act of 1972, waters that fail to meet water quality standards are considered impaired. The Water Quality Index integrates complex water quality data into a readily understood scale. The Benthic Index of Biotic Integrity measures the abundance and diversity of macroinvertebrates in a streambed. Also known as stream bugs, these creatures are a critical part of the aquatic food web and are sensitive to changes in the environment.</p>
+</div>
 			
 			<!--*********************************FOLD 04*********************************-->
 			<h3><a href="#!">What you can do</a></h3>
 			<div>
-				<ol>
-					<li>Use Low Impact Development (LID) techniques to manage <a href="http://www.psp.wa.gov/stormwater.php">stormwater runoff</a> such as natural landscaping, <a href="http://raingarden.wsu.edu/">rain gardens</a>, rain barrels, green roofs and permeable paving where appropriate.</li>
-					<li>Let the Rain Soak In: Slow stormwater runoff by directing downspouts into lawns, beds or <a href="http://raingarden.wsu.edu/">rain gardens</a>. Plant more trees to help capture rainwater.</li>
-					<li>Do not use pesticides on your lawn. Pesticides are poisoning some urban streams.</li>
-					<li><a href="http://pugetsoundstartshere.org/in-your-local-area/get-involved/">Volunteer in your area</a> to work with a group helping to reduce fresh water pollution.</li>
-					<li><a href="http://www.wsdot.wa.gov/choices/">Debris and pollution from cars affect water quality. Try <strong>alternatives to driving</strong> such as public transportation, carpooling, vanpooling, walking, or biking.</a></li>
-					<li>If you do have a car, wash your car at a commercial car wash so wastewater plants can treat the water before it enters local waterways. Learn more at Puget Sound Starts here. </li>
-					<li>Dispose of oil and other auto wastes at your local <a href="https://fortress.wa.gov/ecy/hwfacilitysearch/Forms/SiteSearch.aspx">recycling location or hazardous waste collection facility</a>. Some county-based links are available <a href="county_hazard_waste.php">here</a>.</li>
-				</ol>
+				<ol><li>Use Low Impact Development (LID) techniques to manage stormwater runoff such as natural landscaping, <a href="http://raingarden.wsu.edu">rain gardens,</a> rain barrels, green roofs and permeable paving where appropriate.</li>
+<li>Let the Rain Soak In: Slow stormwater runoff by directing downspouts into lawns, beds or <a href="http://raingarden.wsu.edu">rain gardens.</a> Plant more trees to help capture rainwater.
+<li>Do not use pesticides on your lawn. Pesticides are poisoning some urban streams.</li>
+<li><a href="http://www.pugetsoundstartshere.org/local-resources/get-involved/">Volunteer in your area</a> to work with a group helping to reduce fresh water pollution.</li>
+<li>Debris and pollution from cars affect water quality. <a href="http://www.wsdot.wa.gov/choices/">Try alternatives to driving</a> such as public transportation, carpooling, vanpooling, walking, or biking.</li>
+<li>If you do have a car, wash your car at a commercial car wash so wastewater plants can treat the water before it enters local waterways. Learn more at Puget Sound Starts here.</li>
+<li>Dispose of oil and other auto wastes at your local <a href="https://fortress.wa.gov/ecy/hwfacilitysearch/Forms/SiteSearch.aspx">recycling location or hazardous waste collection facility.</a></li></ol>
+
+
 			</div>
 			<!--*********************************FOLD 05*********************************-->
 			<h3><a href="#!">What our partners are doing</a></h3>
--- fresh_water_quality_indicator2.php
+++ fresh_water_quality_indicator2.php
@@ -3,12 +3,12 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta name="google-site-verification" content="lMaE6uVdAY_4P-zfoagi1Kj2QQLS8W3JvahLqMZMuDU" />
-<title>Puget Sound Partnership - Vital Signs - Fresh Water Quality</title>
-<link href="css/vital1.css" rel="stylesheet" type="text/css" />
+<title>Puget Sound Partnership - Vital Signs - Birds</title>
+<link href="css/vital2016.css" rel="stylesheet" type="text/css" />
 <link type="text/css" href="css/custom-theme/jquery-ui-1.9.2.custom.min.css" rel="stylesheet" />
 <script type="text/javascript" src="js/jquery-1.8.3.js"></script>
 <script type="text/javascript" src="js/jquery-ui-1.9.2.custom.min.js"></script>
-<script type="text/javascript" src="js/divhide.js"></script>
+<script type="text/javascript" src="js/settab.js"></script>
 <script>
 $(function() {
 		$( "#accordion" ).accordion( { collapsible: true }, { active: false }, { autoHeight: false });
@@ -21,48 +21,18 @@ $(function() {
   });
 });	
 </script>
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 3;
+	color = "aqua";
+</script>
 </head>
 <body id="id19" >
 <div id="container">
 	<?php include 'includes/header_test.html' ?>
-	<!--*********************************TITLE BAR BEGIN*********************************-->
-	<div id="roundcornerstop"><img src="img/rounded_top.jpg" width="960" height="20" /></div>
-	<div id="vitalheadercontainer">
-		<div id="vitalheaderleftcontainer">
-			<div id="vitaltitle">Freshwater Quality </div>
-			<div id="vitalquote">21% of stream and river segments that were assessed in 2010 were listed as impaired</div>
-			<div id="vitalchampion">Indicator Leads: Markus Van Prause, Washington State Department of Ecology, Ken Koch, Washington State Department of Ecology, and Jo Wilhelm, King County</div>
-			<div id="vitalbuttonscontainer">
-				<div id="button1"><a href="http://www.psp.wa.gov/pugetsoundprojectatlas" target="_blank">View Projects</a></div>
-				<div id="button2"><a href="http://gismanager.rco.wa.gov/ntaportal?ActionAgenda=2014-2016&Quarter=10&Vital%20Sign=Freshwater%20Quality" target="_blank">View Report Card</a></div>
-			</div>
-		</div>
-		<div id="vitalheaderrightphoto">
-			<div class="underconstruction">This webpage is currently being updated. For the most recent findings for this Vital Sign, please consult the report in the latest <a href="https://pspwa.box.com/s/rhb2s6c2yzb2eskla1ssj2w84vcay7un">State of the Sound</a></div>
-			<div class="photoCredit padding-top">Data last updated on March 6, 2014<br />
-			Photo Credit: Dan Bennett</div></div>
-	</div>
-	<div id="roundcornerbottom"><img src="img/rounded_rounded_bottom.jpg" width="960" height="20" /></div>
-	<!--*********************************TITLE BAR END*********************************--> 
-	<!--*********************************TAB BEGIN*********************************-->
+    <?php include 'includes/freshwater_quality-title.html' ?>
+<?php include 'includes/freshwater_quality-tab.html' ?>	
 	
-	<ul id="nav_indicator">
-		<li><a href="fresh_water_quality.php">Vital Sign: Overview<br />
-			<br />
-			<br />
-			</a> </li>
-		<li ><a href="fresh_water_quality_indicator1.php">Indicator: Water Quality Index<br />
-			<br />
-			</a></li>
-		<li class="indicator_active_aqua"><a href="fresh_water_quality_indicator2.php">Indicator: Number of impaired waters<br />
-			<br />
-			</a></li>
-		<li><a href="fresh_water_quality_indicator3.php">Indicator: Benthic Index of Biotic Integrity<br />
-			<br />
-			</a></li>
-	</ul>
-	<div class="clearfloat"></div>
-	<!--*********************************TAB END*********************************-->
 	<div id="accordioncontainer">
 		<div id="accordion"> 
 			
@@ -71,29 +41,31 @@ $(function() {
 			<div> 
 				<!--*********************************AQUA HEADER*********************************-->
 				<div class="aquaBarHeader">What is the indicator? </div>
-				<p>The number of impaired waters .</p>
-				<h4 class="aqua">Number of impaired waters</h4>
-				<p>Impaired waters are segments of streams, rivers, or lakes that do not meet the State of Washington’s Water Quality Standards for bacteria, dissolved oxygen, temperature, toxics, or other pollutants. Cool, clean water is a key ingredient for a healthy Puget Sound. When lakes and streams have a reduced ability to support native species and human uses, then they are listed as Impaired.</p>
-				<p>The Washington State Department of Ecology reviews data from a variety of sources every four years to identify impairments. The data used to list segments as impaired must meet rigorous data quality standards as outlined in Washington’s Water Quality Policy 1-11.</p>
-				<p>Under the Federal Clean Water Act of 1972, waters are considered impaired when they fail to meet water quality standards or minimum requirements for certain uses. Every two years, states are required to prepare a list of water bodies that do not meet water quality standards. This list is called the 303(d) list, because the process is described in Section 303(d) of the Clean Water Act. To achieve this goal, the State of Washington established water quality standards designed to protect and restore water quality for drinking, recreation, and habitat for fish and other aquatic life.</p>
-				<p>More than one segment of a river can be listed as impaired, and a single segment can be listed for more than one pollutant. Once a segment is listed as impaired, a plan must be created and implemented to control pollution or improve water quality. The effects of these restoration programs can take many years to have a positive impact. </p>
-				
+			
+				  <h5>This indicator is the number of impaired waters.</h5>
+				  <p>When lakes and streams have a reduced ability to support native species and human uses, then they are listed as Impaired. Impaired waters are segments of streams, rivers, or lakes that do not meet the State of Washington&rsquo;s Water Quality Standards for bacteria, dissolved oxygen, temperature, toxics, or other pollutants. Cool, clean water is a key ingredient for a healthy Puget Sound. Under the Federal Clean Water Act of 1972, waters are considered impaired when they fail to meet water quality standards or minimum requirements for certain uses. Every two years, states are required to prepare a list of water bodies that do not meet water quality standards. This list is called the 303(d) list, because the process is described in Section 303(d) of the Clean Water Act. To achieve this goal, the State of Washington established water quality standards designed to protect and restore water quality for drinking, recreation, and habitat for fish and other aquatic life.</p>
+				  <p>More than one segment of a river can be listed as impaired, and a single segment can be listed for more than one pollutant. Once a segment is listed as impaired, a plan must be created and implemented to control pollution or improve water quality. The effects of these restoration programs can take many years to have a positive impact.</p>
+			  <p>The Washington State Department of Ecology reviews data from a variety of sources to identify impairments relative to strict standards. Each freshwater listing is a unique combination of an assessment unit (a segment of river), medium (water, tissue, or sediment), and parameter (temperature, bacteria, or dissolved oxygen). Monitoring multiple parameters at several stations along the length of shoreline typically results in multiple assessment units, each of which will have a listing developed for each monitored parameter. The possible outcomes of the water quality assessment under the Clean Water Act 303(d) section are:</p>
+				  <blockquote>
+				    <p>Category 1: Meets Water Quality Standards<br />
+				      Category 2: Water of Concern<br />
+				      Category 3: Insufficient Data<br />
+				      Category 4A: Total Maximum Daily Loads have been established for the identified impairment<br />
+				      Category 4B: Impaired but an alternative pollution control plan is in place<br />
+				      Category 4C: Impaired by non-pollutant<br />
+				      Category 5: Impaired by a pollutant or an unidentified cause</p>
+		      </blockquote>
+
 				<!--*********************************AQUA HEADER*********************************-->
-				<div class="aquaBarHeader">What is the target? </div>
-				<h5>2020 target</h5>
+			  <div class="aquaBarHeader">What are the targets? </div>
+				<h5>2020 target:</h5>
 				<ul>
 					<li>Reduce the number of “impaired” waters.</li>
 				</ul>
 				<h5>Interim targets</h5>
 				<p>Interim targets have not been set for this indicator</p>
 				
-				<!--*********************************AQUA HEADER*********************************-->
-				<div class="aquaBarHeader">Is there progress? </div>
-				<p>&nbsp;</p>
-				<h4 class="aqua">Number of impaired waters</h4>
-				<p>Although the number of impairments for rivers and streams decreased by 77 segments in 2010 - a step in the right direction - it does not mean that these segments now meet water quality standards. Instead, the change in number of impairments was largely due to the number of segments receiving approval for their water quality improvement project plans or pollution control programs. </p>
-				<p>Having a plan in place removes a segment from the impairment list, but does not necessarily mean that the area has been restored or that water quality standards are being met. For example, only four segments from the 2010 list were removed from the impaired list because they met water quality standards. </p>
-				<p>New data for freshwater were not reviewed in 2010; the 2012 water quality assessment will use new data and be published sometime in 2013. The number of freshwater impairments is likely to rise significantly in 2012 due to an increase in data and the number of sites assessed. Comparing the number of impairments for 2008 to 2012 will be difficult because the method used to map and count segments will change. </p>
+				
 			</div>
 			
 			<!--*********************************FOLD 03*********************************-->
@@ -101,16 +73,18 @@ $(function() {
 			<div> 
 				<!--*********************************AQUA HEADER*********************************-->
 				<div class="aquaBarHeader">Data</div>
-				<h4 class="aqua">Number of impaired waters</h4>
-				<img src="images/freshwater_chart2.png" width="800" height="880" alt="Number of streams and river segments listed in each assessment category, 2008 and 2010" />
-				<p>In the Puget Sound basin, the 2010 Water Quality Assessment showed a total of 6,957 segment and parameters combinations were assessed. A total of 1,496 river and stream segments, in 525 rivers and streams, did not meet Water Quality Standards and thus were listed as impaired.</p>
-				<p>Impairments occurred in all 19 Water Resource Inventory Areas (WRIAs) in the Puget Sound basin (Figures 5 to 7). More than 60% of the total number of listings for Puget Sound rivers and streams were in five watersheds: Nooksack (296 listings), Kitsap (194), Cedar/Sammamish (181), Duwamish-Green (132), and Lower Skagit-Samish (109). For Puget Sound lakes, 52 were listed as impaired; 48% were listed for bacteria and total phosphorus, and approximately one-half were listed for toxic chemical contamination.</p>
-				<p>The most frequently cited data for listing segments as impaired were bacteria (524 listings), dissolved oxygen (460), temperature (353), and pH (97). However, the largest number of segments (39%) could not be categorized because of insufficient data. Water Quality Standards include strict rules about the number of samples required to determine whether a segment is impaired or meeting standards.</p>
-				<p>Segments listed as waters of concern have data that indicate a problem, but not enough data to make a determination of impairment.</p>
-				<p>Sampling of streams, rivers, and lakes tends to focus in areas with known problems; therefore, not all segments have been assessed, and some impairments can be missed. Consequently, impairment data are not a complete reflection of the overall health of all streams, rivers, and lakes in Puget Sound watersheds.</p>
-				<p>In addition, selection of monitoring sites is frequently constrained by funding. Monitoring efforts are split between monitoring established sites and looking for new problems. This limits the numbers of new waters that are addressed during a cycle.</p>
-				<div class="aquaBarHeader">Maps</div>
-				<img src="images/freshwater_chart5.png" width="567" height="675" alt="River and stream segments listed as impaired for bacteria" /> <img src="images/freshwater_chart6.png" width="584" height="677" alt="River and stream segments listed as impaired for dissolved oxygen" /> <img src="images/freshwater_chart7.png" width="597" height="800" alt="River and stream segments listed as impaired for temperature" /> </div>
+                <h5>Is the indicator making progress toward the 2020 target?</h5>
+				<p><span><img src="images/status_no_data.png" width="118" height="121" alt=""/>No data are available yet to assess progress.</span></p>
+				<p>Since 1988, water quality has been assessed in approximately 10 percent of the waters of Washington State. The 2014 assessment report contains 10,175 listings for freshwaters in the Puget Sound region. Of these 1,694 listings (about 17 percent) are in Category 5, per the Candidate 303(d) list. An additional 741 listings also do not meet water quality standards but are in Categories 4A and 4B because they have an established cleanup plan. However, most listings (4,583) have insufficient data to determine the water quality status. These are unofficial results and are pending approval by the U.S. Environmental Protection Agency.</p>
+<ul><li>To examine the change in listing rates over time, a subset of listings whose water quality status was reported on in both the 2004 and 2014 assessments was used (a total of 2,071 listings). The status of 68 listings changed category from impaired (Categories 5, 4A, or 4B) to non-impaired (Category 1). For most of these de-listings, the improvement in water quality is the result of the successful implementation of pollution control practices; in particular, efforts to reduce fecal bacteria pollution.</li>
+<li>Another sign of progress is that since 2004, pollution control plans have been developed for 236 of the new or ongoing 303(d) listings in efforts to restore water quality.</li>
+<li>However, a greater number of listings (392) have moved from a non-impaired status to an impaired status between assessments. Most of these new 303(d) listings are due to additional data that have improved the characterization of water quality conditions rather than indicating worsening water quality.</li>
+<li>The fate of the majority of listings was unchanged and remained impaired (852). An absence of change in category between assessments may be due to following: newer data verifying no water quality change; newer data that are insufficient to justify a category change; or no new data collected since 2004 (a category carries through time until newer data justifies a category change). Data about water quality are not systematically collected and submitted for each body of water with a listing. The assessments were not designed to answer questions about the status and trends of water quality. Therefore, reporting meaningfully on the progress of this indicator is not possible at this time.</li></ul>
+<h5>FIGURE 1.</h5>
+<p><img src="images/freshwater.png" /></p>
+<p><em>Source: Washington State Department of Ecology, Water Quality Program</em> </p>
+<div class="aquaBarHeader">Maps</div>
+			</div>
 			
 			<!--*********************************AQUA HEADER*********************************--> 
 			
