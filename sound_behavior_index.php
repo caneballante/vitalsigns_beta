613c05dc302640be0e58dc60032676d97363a181
Erika Lari <erika.lari@psp.wa.gov>
2016-06-16 11:56:34 -0700

    sound stewardship

--- sound_behavior_index.php
+++ sound_behavior_index.php
@@ -3,12 +3,12 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta name="google-site-verification" content="lMaE6uVdAY_4P-zfoagi1Kj2QQLS8W3JvahLqMZMuDU" />
-<title>Puget Sound Partnership - Vital Signs - Sound Behavior Index</title>
-<link href="css/vital1.css" rel="stylesheet" type="text/css" />
+<title>Puget Sound Partnership - Vital Signs - Sound Stewardship</title>
+<link href="css/vital2016.css" rel="stylesheet" type="text/css" />
 <link type="text/css" href="css/custom-theme/jquery-ui-1.9.2.custom.min.css" rel="stylesheet" />
 <script type="text/javascript" src="js/jquery-1.8.3.js"></script>
 <script type="text/javascript" src="js/jquery-ui-1.9.2.custom.min.js"></script>
-<script type="text/javascript" src="js/divhide.js"></script>
+<script type="text/javascript" src="js/settab.js"></script>
 <script>
 $(function() {
 		$( "#accordion" ).accordion( { collapsible: true }, { active: false }, { autoHeight: false });
@@ -21,42 +21,17 @@ $(function() {
   });
 });	
 </script>
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 1;
+	color = "orange";
+</script>
 </head>
 <body id="id05" >
 <div id="container">
 	<?php include 'includes/header_test.html' ?>
-	<!--*********************************TITLE BAR BEGIN*********************************-->
-	<div id="roundcornerstop"><img src="img/rounded_top.jpg" width="960" height="20" /></div>
-	<div id="vitalheadercontainer">
-		<div id="vitalheaderleftcontainer">
-			<div id="vitaltitle">Sound Behavior</div>
-			<div id="vitalquote"> Many of our common day-to-day behaviors and practices seem benign on their own, but, when multiplied by 4.5 million residents, their cumulative effects can harm Puget Sound</div>
-			<div id="vitalchampion"> Indicator lead: Dave Ward, Puget Sound Partnership </div>
-			<div id="vitalbuttonscontainer">
-				<div id="button1"><a href="http://www.psp.wa.gov/pugetsoundprojectatlas" target="_blank">View Projects</a></div>
-				<div id="button2"><a href="http://gismanager.rco.wa.gov/ntaportal/?VitalSign=Sound+Behavior+Index" target="_blank">View Report Card</a></div>
-			</div>
-		</div>
-		<div id="vitalheaderrightphoto">
-			<div class="underconstruction">This webpage is currently being updated. For the most recent findings for this Vital Sign, please consult the report in the latest <a href="https://pspwa.box.com/s/rhb2s6c2yzb2eskla1ssj2w84vcay7un">State of the Sound</a></div>
-			<div class="photoCredit padding-top">Data last updated onApril 24, 2015</div></div>
-	</div>
-	<div id="roundcornerbottom"><img src="img/rounded_rounded_bottom.jpg" width="960" height="20" /></div>
-	
-	<!--*********************************TAB BEGIN*********************************-->
-	
-	<ul id="nav_indicator">
-		<li class="indicator_active_orange"><a href="sound_behavior_index.php">Vital Sign Overview<br />
-			<br />
-			<br />
-			</a> </li>
-		<li><a href="sound_behavior_index_indicator1.php">Indicator: Sound Behavior Index<br />
-			<br />
-			</a></li>
-	</ul>
-	<div class="clearfloat"></div>
-	
-	<!--*********************************TITLE BAR BEGIN*********************************-->
+	<?php include 'includes/sound_stewardship-title.html' ?>
+	<?php include 'includes/sound_stewardship-tab.html' ?>
 	<div id="accordioncontainer">
 		<div id="accordion"> 
 			
@@ -64,10 +39,9 @@ $(function() {
 			
 			<h3><a href="#!">Importance to Puget Sound recovery</a></h3>
 			<div>
-				<p>Many of our common day-to-day behaviors and practices seem benign on their own, but when multiplied by 4.5 million residents, their cumulative effects can harm Puget Sound. Reversing this trend - fostering beneficial behaviors and reducing detrimental ones - is a crucial step in Puget Sound’s recovery.</p>
-				<p>The Sound Behavior Index tracks 28 specific practices that can affect water quality and aquatic habitat. The index is based on a survey conducted every two years among a statistical sample of Puget Sound residents. It asks them about specific, measurable, repetitive behaviors that affect water quality and aquatic health.</p>
-				<p>The Sound Behavior Index distills the region’s environmental performance into a single score, which can be tracked across time. By measuring long-term shifts in behaviors and practices across the Puget Sound region, the index gives policy makers a tool to set priorities for regional and local programs and baseline from which to measure progress.</p>
-			</div>
+				<p>Fostering beneficial behaviors and reducing detrimental ones is a crucial step in Puget Sound&rsquo;s recovery.</p>
+			    <p>The Sound Behavior Index tracks 28 specific practices that can affect water quality and aquatic habitat. The index is based on a survey conducted every two years among a statistical sample of Puget Sound residents. It asks them about specific, measurable, repetitive behaviors that affect water quality and aquatic health.</p>
+</div>
 			
 			<!--*********************************FOLD 05*********************************-->
 			<h3><a href="#!">What our partners are doing</a></h3>
